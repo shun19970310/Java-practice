6591de1de17ab9e418dac1f4ec939390d890defd
shun19970310 <shunma19970310@gmail.com>
2022-07-12 13:35:39 +0000

    [Finish]java1-3

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+
+  public static void main(String[] args) {
+        final int LUCKY_NUMBER = 1;
+        String str = "人事を尽くして天命を待つ";
+        System.out.println(str);
+        str = "塵も積もれば山となる";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+  }
+
+}
