a4afae68afb7db6a73238d6ddb4ec49f53852587
shun19970310 <shunma19970310@gmail.com>
2022-07-19 10:41:29 +0000

    [First]ALH.Challenge1

---
+++ Unit01.java
@@ -0,0 +1,11 @@
+public class Unit01 {
+    public static void main(String[] args) {
+		  int sum = 0;
+
+		  for(int i = 1; i <= 100; i ++) {
+			  sum += i;
+		  }
+
+		  System.out.println("1から100までの整数の合計は" + sum + "です。");
+  	}
+}
